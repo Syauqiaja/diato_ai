84327e8a624d12571ad78f936729be442d30861e
mac <fajrudinsauqi@gmail.com>
2026-04-05 23:59:15 +0700

    feat: add tim pengembang

--- lib/core/assets/assets.dart
+++ lib/core/assets/assets.dart
@@ -1,4 +1,9 @@
 abstract class Assets {
   static const String cellBg = "assets/cell-bg.png";
   static const String diatomi = "assets/diatomi.jpg";
+
+  static const String imgFahmi = "assets/fahmi.jpg";
+  static const String imgIbrohim = "assets/ibrohim.jpg";
+  static const String imgSusriyati = "assets/susriyati.jpg";
+  static const String imgZubaidah = "assets/zubaidah.jpg";
 }
--- lib/features/setting/presentation/developer_info_screen.dart
+++ lib/features/setting/presentation/developer_info_screen.dart
@@ -1,3 +1,4 @@
+import 'package:diato_ai/core/assets/assets.dart';
 import 'package:diato_ai/core/theme/theme.dart';
 import 'package:diato_ai/features/shared/widgets/spacings.dart';
 import 'package:diato_ai/utils/extensions/context_extensions.dart';
@@ -47,6 +48,8 @@ class DeveloperInfoScreen extends StatelessWidget {
                 label: null,
                 content: _content
               ),
+              vSpace(16),
+              _DeveloperTeamSection(developers: _developers),
             ],
           ),
         ),
@@ -54,6 +57,119 @@ class DeveloperInfoScreen extends StatelessWidget {
     );
   }
 }
+
+class _DeveloperTeamSection extends StatelessWidget {
+  final List<_DeveloperData> developers;
+
+  const _DeveloperTeamSection({required this.developers});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: AppTheme.canvasColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Tim Pengembang',
+              style: context.textTheme.titleMedium?.copyWith(
+                color: context.colorScheme.primary,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            vSpace(12),
+            GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                mainAxisExtent: 280,
+              ),
+              itemCount: developers.length,
+              itemBuilder: (context, index) {
+                final developer = developers[index];
+                return _DeveloperCard(developer: developer);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _DeveloperCard extends StatelessWidget {
+  final _DeveloperData developer;
+
+  const _DeveloperCard({required this.developer});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Align(
+          alignment: Alignment.topCenter,
+          child: AspectRatio(
+            aspectRatio: 3 / 4,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                developer.imagePath,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+        vSpace(8),
+        Text(
+          developer.name,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          textAlign: TextAlign.center,
+          style: context.textTheme.bodyMedium?.copyWith(
+            color: context.colorScheme.primary,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _DeveloperData {
+  final String name;
+  final String imagePath;
+
+  const _DeveloperData({
+    required this.name,
+    required this.imagePath,
+  });
+}
+
+const List<_DeveloperData> _developers = [
+  _DeveloperData(
+    name: 'M. Iqbal Najib Fahmi, M.Pd',
+    imagePath: Assets.imgFahmi,
+  ),
+  _DeveloperData(
+    name: 'Prof. Dr. Susriyati Mahanal, M.Pd',
+    imagePath: Assets.imgSusriyati,
+  ),
+  _DeveloperData(
+    name: 'Prof. Dr. Siti Zubaidah, M.Pd',
+    imagePath: Assets.imgZubaidah,
+  ),
+  _DeveloperData(
+    name: 'Prof. Dr. Ibrohim, M.Si',
+    imagePath: Assets.imgIbrohim,
+  ),
+];
 
 const String _content = 
 """Diatom-AI dikembangkan oleh tim akademisi bidang pendidikan biologi dan ekologi perairan dari Universitas Negeri Malang. 
